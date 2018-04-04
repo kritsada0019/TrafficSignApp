46905642bd10364ea2cd451940db27087a751e17
krtsada <emmypinklab@gmail.com>
2018-04-04 10:16:30 +0700

    Intent Detail Complete

--- app/src/main/java/sdu/cs58/kritsada/trafficsignapp/DetailActivity.java
+++ app/src/main/java/sdu/cs58/kritsada/trafficsignapp/DetailActivity.java
@@ -2,15 +2,28 @@ package sdu.cs58.kritsada.trafficsignapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
+    TextView titleTextView,detailTextView;
+    ImageView signImageView;
+    Button backButton;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+        titleTextView = findViewById(R.id.txvTitle);
+        detailTextView = findViewById(R.id.txvDetail);
+        signImageView = findViewById(R.id.imvSign);
 
-
-
+        //รับข้อมูลจาก Intent ไปแสดงบนหน้า XML
+        titleTextView.setText(getIntent().getStringExtra("Title"));
+        detailTextView.setText(getIntent().getStringExtra("Detail"));
+        signImageView.setImageResource(getIntent().getIntExtra("Image",R.drawable.traffic_01));
     }//end Oncreate
 }//end class
